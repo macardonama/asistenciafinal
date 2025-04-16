f5085190420d9c65288bb88b304142ea4d56c082
macardonama <macardonama@gmail.com>
2025-04-16 14:56:16 -0500

    corrige el selector de componente raiz

--- src/app/attendance/attendance.component.ts
+++ src/app/attendance/attendance.component.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 @Component({
-  selector: 'app-attendance',
+  selector: 'app-root',
   standalone: true,
   imports: [CommonModule, HttpClientModule, FormsModule],
   templateUrl: './attendance.component.html',
