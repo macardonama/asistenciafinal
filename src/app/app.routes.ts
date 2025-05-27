9ad6e80db31380843fe49842e3467d0015d744be
CristinaGM4 <m.guapacha@uniandes.edu.co>
2025-05-26 21:26:34 -0500

    Modifica app.component.html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,9 @@
 import { Routes } from '@angular/router';
+import { DiarioAulaComponent } from './diario-aula/diario-aula.component';
+import { AttendanceComponent } from './attendance/attendance.component';
 
 export const routes: Routes = [
-  {
-    path: '',
-    redirectTo: '/diario-aula',
-    pathMatch: 'full'
-  },
-  {
-    path: 'diario-aula',
-    loadComponent: () =>
-      import('./diario-aula/diario-aula.component').then(m => m.DiarioAulaComponent)
-  },
-  {
-    path: 'asistencia',
-    loadComponent: () =>
-      import('./attendance/attendance.component').then(m => m.AttendanceComponent)
-  }
+  { path: '', redirectTo: 'attendance', pathMatch: 'full' },
+  { path: 'attendance', component: AttendanceComponent },
+  { path: 'diario-aula', component: DiarioAulaComponent },
 ];
