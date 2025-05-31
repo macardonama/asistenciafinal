1ac13d9a0af21799207ece41b416a3a54ff4bce7
CristinaGM4 <m.guapacha@uniandes.edu.co>
2025-05-31 13:53:14 -0500

    Corrige error de componente Inicio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,21 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './inicio.component';
-import { DiarioAulaComponent } from './diario-aula/diario-aula.component';
-import { AttendanceComponent } from './attendance/attendance.component';
 
 export const routes: Routes = [
   { path: '', component: InicioComponent },
-  { path: 'attendance', component: AttendanceComponent },
-  { path: 'diario-aula', component: DiarioAulaComponent },
   {
-  path: 'evaluacion-estudiante',
-  loadComponent: () => import('./components/evaluacion-estudiante.component').then(m => m.EvaluacionEstudianteComponent)
-}
+    path: 'asistencia',
+    loadComponent: () =>
+      import('./attendance/attendance.component').then(m => m.AttendanceComponent)
+  },
+  {
+    path: 'diario-aula',
+    loadComponent: () =>
+      import('./diario-aula/diario-aula.component').then(m => m.DiarioAulaComponent)
+  },
+  {
+    path: 'evaluacion-estudiante',
+    loadComponent: () =>
+      import('./components/evaluacion-estudiante.component').then(m => m.EvaluacionEstudianteComponent)
+  }
 ];
