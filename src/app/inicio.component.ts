6fcd13a2d57fdde6b8c1aa0e79474675cc220b39
CristinaGM4 <m.guapacha@uniandes.edu.co>
2025-05-31 14:22:04 -0500

    Corrige router de Inicio.component

--- src/app/inicio.component.ts
+++ src/app/inicio.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule], // <-- IMPORTANTE
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
-export class InicioComponent {
-
-}
+export class InicioComponent {}
