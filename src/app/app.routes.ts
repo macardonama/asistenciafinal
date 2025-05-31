854c8d24460a83672761ba82bb3633422f218965
CristinaGM4 <m.guapacha@uniandes.edu.co>
2025-05-31 13:42:13 -0500

    Agrega nuevo componente llamado Inicio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './inicio.component';
 import { DiarioAulaComponent } from './diario-aula/diario-aula.component';
 import { AttendanceComponent } from './attendance/attendance.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'attendance', pathMatch: 'full' },
+  { path: '', component: InicioComponent },
   { path: 'attendance', component: AttendanceComponent },
   { path: 'diario-aula', component: DiarioAulaComponent },
   {
