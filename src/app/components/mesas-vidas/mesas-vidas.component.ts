4bcbea6d91d4b155d23a8fab548a5110bed88ec6
CristinaGM4 <m.guapacha@uniandes.edu.co>
2025-06-08 18:34:03 -0500

    Agrega mesas y vidas

--- src/app/components/mesas-vidas/mesas-vidas.component.ts
+++ src/app/components/mesas-vidas/mesas-vidas.component.ts
@@ -10,17 +10,27 @@ export class MesasVidasComponent implements OnInit {
   mesas: any[] = [];
 
   ngOnInit(): void {
-    const guardado = localStorage.getItem('estadoMesas');
-    if (guardado) {
+  const guardado = localStorage.getItem('estadoMesas');
+  if (guardado) {
+    try {
       this.mesas = JSON.parse(guardado);
-    } else {
-      this.mesas = Array.from({ length: 15 }, (_, i) => ({
-        numero: i + 1,
-        vidas: 5
-      }));
-      this.guardarEstado();
+    } catch (e) {
+      console.error('Error al leer localStorage, reiniciando...', e);
+      this.inicializarMesas();
     }
+  } else {
+    this.inicializarMesas();
   }
+}
+
+inicializarMesas() {
+  this.mesas = Array.from({ length: 15 }, (_, i) => ({
+    numero: i + 1,
+    vidas: 5
+  }));
+  this.guardarEstado();
+}
+
 
   perderVida(mesa: any) {
     if (mesa.vidas > 0) {
